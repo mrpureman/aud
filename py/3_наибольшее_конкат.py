86c816ab75bc37e21e20c953650c6d127b726af2
mrpureman <mrpureman@yandex.ru>
2024-06-26 00:14:26 +0300

    upd

--- py/3_наибольшее_конкат.py
+++ py/3_наибольшее_конкат.py
@@ -1,58 +1,43 @@
-
-
 def getMaxNum(listOfNums):
-    #listOfNums = sorted(listOfNums)
-    # to Int
-    listOfNumsInt = [int(item) for item in listOfNums] 
-    #print(listOfNums)
 
+    # в число
+    listOfNumsInt = [int(item) for item in listOfNums] 
+
+    # сколько разрядов?
     lenNums = max([len(str(item)) for item in listOfNumsInt])
     tempList = []
-   # print(lenNums)
-
-
-    zeroIndexes = []
     templist2 = []
+
+    # добавляем нули к числу
     for i in listOfNums:
         if len(i) < lenNums:
             tempList.append(i + ((lenNums - len(i)) *'0'))
-
-            zeroIndexes.append(int(str(1)  + ((lenNums - len(i)) *'0')))
+            
+            # запоминаем число и его делитель, чтобы потом разделить
             templist2.append([int(i + ((lenNums - len(i)) *'0')), int(str(1)  + ((lenNums - len(i)) *'0'))])
         else:
             tempList.append(i)
-            zeroIndexes.append(1)
-            
+
+    # сортируем         
     tempList = sorted(tempList, reverse=1)
-    
-    print(lenNums)
-    print(zeroIndexes)
-    
+
     listOfNums = []
-    print("templis2= " + str(templist2))
+
+    # Если число есть — делим его на 10, 100, 1000 (зависит от количества разрядов)
     for i in tempList:
-        x = int(i)
+        x = i
         for j in templist2:
         
             if int(i) == j[0]:
                 x = round(j[0] / j[1])
                 
-
         listOfNums.append(x)
-           
-
-
-    return listOfNums # "".join(map(str, listOfNums))
-
-
-    
-
-
-
+          
+    return "".join(map(str, listOfNums))
 
 def main():
-    nums = ['562', '03', '83'] #, '05', '67', '004']
+    #nums = ['41', '4', '005' , '89'] #, '05', '67', '004']
+    nums = ['03', '25', '1' , '07'] #, '05', '67', '004']
     print(getMaxNum(nums))
-
 
 main()
