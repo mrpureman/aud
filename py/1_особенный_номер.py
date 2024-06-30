803b7d1d585630370e5ce5bd97bd2e9f653f5249
mrpureman <mrpureman@yandex.ru>
2024-07-01 01:06:59 +0300

    Update 1_особенный_номер.py

--- py/1_особенный_номер.py
+++ py/1_особенный_номер.py
@@ -2,7 +2,10 @@ import re
 
 def getGoodNum(srcStr):
     outStr = ""
+    # ищем совпадения регуляркой. Принимаем только 2-4\2-5 числа.
     findNums = re.findall(r'(\b\d{2,4}\b)\\(\b\d{2,5}\b)', srcStr)
+    
+    # перебираем и форматируем числа, скидываем в строку.
     for i in findNums:      
        outStr+= f'{int(i[0]):04}' + "\\"+f'{int(i[1]):05}' + "\n"
     return outStr
